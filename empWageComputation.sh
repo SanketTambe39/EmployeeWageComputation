4c6f6269595eaf7983153c6a61777910980afbfb
SanketTambe39 <sankettambe39@gmail.com>
2020-08-06 17:51:37 +0530

    Checking Daily Wage of an employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,13 +2,17 @@
 
 echo " Welcome to Employee Wage Computation program on Master Branch "
 
-
 isPresent=1
 randomCheck=$((RANDOM%2))
 
 if [ $isPresent -eq $randomCheck ]
 then
-        echo "Employee is present"
+	empRatePerHr=20
+	empHrs=8
+	salary=$(($empHrs*$empRatePerHr))
+	echo "Emplyee is present So his Daily salary is ::"$salary
 else
-        echo "Employee is absent"
+	salary=0
+	echo " Employee is Absent so no salary "
 fi
+
