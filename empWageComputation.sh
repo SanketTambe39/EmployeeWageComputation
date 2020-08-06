4a21645e548897ddca4f86eadc6ada2d48577c0c
SanketTambe39 <sankettambe39@gmail.com>
2020-08-06 18:52:02 +0530

    Employee Monthly Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,21 +4,24 @@ echo " Welcome to Employee Wage Computation program on Master Branch "
 
 isPartTime=1
 isFullTime=2
+totalSalry=0
 empRatePerHr=20
-randomCheck=$((RANDOM%3))
+NumWorkingDays=20
 
-case $randomCheck in
-	$isFullTime)
+for (( day=1; day<=$NumWorkingDays; day++))
+do
+	randomCheck=$((RANDOM%3))
+
+	if [ $isFullTime -eq $randomCheck ]
+	then
 		empHrs=8
-		;;
-	$isPartTime)
+	elif [ $isPartTime -eq $randomCheck ]
+	then
 		empHrs=4
-		;;
-	*)
+	else
 		empHrs=0
-		;;
-esac
-
-salary=$(($empHrs*$empRatePerHr))
-
-echo " Employee salary is :: "$salary
+	fi
+	sal=$(($empHrs*$empRatePerHr))
+	totalSalary=$(($totalSalary+$sal))
+done
+echo " Employee salary for a month is "$(($totalSalary))
